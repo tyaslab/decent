e19e26b520b4f7e60a8d92432e51e5a972f2d432
tyaslab <tyaslab.com@gmail.com>
2026-05-18 15:41:54 +0700

    published year

--- internal/domain/entity/book.go
+++ internal/domain/entity/book.go
@@ -7,7 +7,7 @@ type Book struct {
 	Title         string    `gorm:"not null;size:200" json:"title" validate:"required,max=200"`
 	Author        string    `gorm:"not null;size:100" json:"author" validate:"required,max=100"`
 	Description   string    `gorm:"size:1000" json:"description" validate:"max=1000"`
-	PublishedYear int       `json:"published_year" validate:"min=1000,max=9999"`
+	PublishedYear int       `json:"year" validate:"min=1000,max=9999"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
 }
